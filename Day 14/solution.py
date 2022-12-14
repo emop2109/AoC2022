8d3fc9557ae57f48ba6d07f4777cf5769224c363
Emil <e.oppegaard@gmail.com>
2022-12-14 14:36:20 +0100

    Day 14

--- Day 14/solution.py
+++ Day 14/solution.py
@@ -1,67 +1,85 @@
 with open('.\input.txt', 'r') as file:
     input = file.read().splitlines()
-input = [[[int(n) for n in x.split(',')]for x in line.split(' -> ')] for line in input]
+input = [[[int(x) for x in xy.split(',')] for xy in line.split(' -> ')] for line in input]
+
+# Generer en liste med startpunkter x,y og en retningsvektor vx, vy
+# Finner også max verdiene i x og y retning
 vectors, max_x, max_y = [], 0, 0
-for i in range(0,len(input)):
+for i in range(0, len(input)):
     v = []
-    for j in range(1,len(input[i])):
-        v.append([input[i][j-1][0], input[i][j-1][1],input[i][j][0]-input[i][j-1][0], input[i][j][1]-input[i][j-1][1]])
-    vectors.append(v)
-for i in range(0,len(input)):
-    for j in range(0,len(input[i])):
+    for j in range(1, len(input[i])):
+        vx, vy = input[i][j][0]-input[i][j-1][0], input[i][j][1]-input[i][j-1][1]
+        v.append([input[i][j-1][0], input[i][j-1][1], vx, vy])
         max_x = input[i][j][0] if input[i][j][0] > max_x else max_x
         max_y = input[i][j][1] if input[i][j][1] > max_y else max_y
+    vectors.append(v)
+# Generer en grid 
+grid = [['.' for i in range(0,max_x+1000)] for n in range(0,max_y+2)]
 
-grid = [['.' for i in range(0,max_x+1)] for n in range(0,max_y+3)]
-
-def drawRock(grid, xy, v):
-    if v[0] == 0:
-        if v[1] < 0:
-            for i in range(xy[1], xy[1]-abs(v[1])-1, -1):
-                grid[i][xy[0]] = '#'
+# Funksjonen tenger # basert på linjene i vektor listen
+def drawRock(grid, v):
+    if v[2] == 0:
+        if v[3] < 0:
+            for i in range(v[1], v[1]+v[3]-1,-1):
+                grid[i][v[0]] = '#'
         else:
-            for i in range(xy[1], xy[1]+abs(v[1])):
-                grid[i][xy[0]] = '#'
+            for i in range(v[1], v[1]+v[3]+1):
+                grid[i][v[0]] = '#'
     else:
-        if v[0] < 0:
-            for i in range(xy[0], xy[0]-abs(v[0])-1, -1):
-                grid[xy[1]][i] = '#'
+        if v[2] < 0:
+            for i in range(v[0], v[0]+v[2]-1,-1):
+                grid[v[1]][i] = '#'
         else:
-            for i in range(xy[0], xy[0]+abs(v[0])):
-                grid[xy[1]][i] = '#'
+            for i in range(v[0], v[0]+v[2]+1):
+                grid[v[1]][i] = '#'
 
-def sandMove(grid, pos):
-    if pos[1]+1 == len(grid):
-        return [pos[0], pos[1]+1]
+# Tegner alle # som finnes i inputen
+for vec in vectors:
+    for v in vec:
+        drawRock(grid, v)
 
-    if grid[pos[1]+1][pos[0]] == '.':
-        return [pos[0],pos[1]+1]
-    elif grid[pos[1]+1][pos[0]-1] == '.':
-        return [pos[0]-1,pos[1]+1]
-    elif grid[pos[1]+1][pos[0]+1] == '.':
-        return [pos[0]+1,pos[1]+1]
+# Funksjonen finner hvilestedet for hvert sandkorn rekursivt. arg=void er er for del 1 hvor
+# sandkornet vil falle i evigheten.
+def sandMove(grid, pos, arg='void'):
+    if pos[1] + 1 == len(grid):
+        if arg == 'void':
+            return False
+        else:
+            grid[pos[1]][pos[0]] = 'o'
+            return True
     else:
-        return None
-def drawSand(grid):
-    position = [500,0]
-    while sandMove(grid, position) is not None:
-        position = sandMove(grid, position)
-    try:
-        grid[position[1]][position[0]] = 'o'
-        return True
-    except:
-        return False
+        if grid[pos[1] + 1][pos[0]] != 'o' and grid[pos[1] + 1][pos[0]] != '#':
+            return sandMove(grid, [pos[0], pos[1] + 1], arg)
+        elif grid[pos[1] + 1][pos[0] - 1] != 'o' and grid[pos[1] + 1][pos[0] - 1] != '#':
+            return sandMove(grid, [pos[0] - 1, pos[1] + 1], arg)
+        elif grid[pos[1] + 1][pos[0] + 1] != 'o' and grid[pos[1] + 1][pos[0] + 1] != '#':
+            return sandMove(grid, [pos[0] + 1, pos[1] + 1], arg)
+        else:
+            grid[pos[1]][pos[0]] = 'o'
+            return True
+# ---------------------------------------- Del 1 -------------------------------------------
+# Teller antall sandkorn som må til før neste sandkorn faller i evigheten
+cnt = 0
+while sandMove(grid, [500, 0]):
+    cnt += 1
+print(cnt)
 
-for line in vectors:
-    for v in line:
-        drawRock(grid, [v[0],v[1]], [v[2],v[3]])
-
-for i in range(0,25):
-    drawSand(grid) 
-
+# Funksjon som skriver en figur av sandkornenes bevegelse
 msg = ''
+grid[0][500] = '+'
 for i in range(0, len(grid)):
-    for j in range(490, 504):
+    for j in range(450, 511):
         msg += grid[i][j]
     msg += '\n'
-print(msg)
+with open('.\output.txt', 'w') as file:
+    file.write(msg)
+
+# ---------------------------------------- Del 2 -------------------------------------------
+# Teller antall sandkorn som må til før et sandkorn finner hvilestedet sitt i (500,0)
+cnt = 0
+while grid[0][500] != 'o':
+    sandMove(grid, [500, 0], arg='NoVoid')
+    cnt += 1
+print(cnt)
+
+
